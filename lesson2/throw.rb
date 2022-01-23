925fb346fa15f119c4605464f6c5757f6cc8c9ec
tenmasato <narito1015@icloud.com>
2022-01-23 11:18:36 +0000

    [Update]

---
+++ lesson2/throw.rb
@@ -0,0 +1,12 @@
+fruits = ['apple','melon','orange']
+numbers = [1,2,3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit},#{n}"
+      if fruit == 'orange' && n == 3
+        throw :foo
+      end
+    end
+  end
+end
