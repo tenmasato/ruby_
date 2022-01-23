8786ffc16675945d7878edbe3893f6caf0237a52
tenmasato <narito1015@icloud.com>
2022-01-23 11:09:37 +0000

    [Finish]

---
+++ lesson2/catch.rb
@@ -0,0 +1,13 @@
+fruits = ['apple','melon','orange']
+numbers = [1,2,3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      if fruit == 'orange' && n == 3
+        #全ての繰り返し処理を脱出する
+        throw :done
+      end
+    end
+  end
+end
