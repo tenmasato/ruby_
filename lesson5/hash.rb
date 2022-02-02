c5dd0f3beda1a7c53be5e26a069284b62e4c1bab
tenmasato <narito1015@icloud.com>
2022-02-02 08:07:00 +0000

    [Update]繰り返し処理

--- lesson5/hash.rb
+++ lesson5/hash.rb
@@ -1,4 +1,7 @@
-#空のハッシュを作成しそのクラス名を確認
-{}.class
+currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
 
-{'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key} : #{value}"
+end
