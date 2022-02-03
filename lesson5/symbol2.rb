28201fe0150462eee396fc416edefe2906fecf5d
tenmasato <narito1015@icloud.com>
2022-02-03 08:26:53 +0000

    [Update]

--- lesson5/symbol2.rb
+++ lesson5/symbol2.rb
@@ -1,6 +1,4 @@
 #ハッシュのキーをシンボルにする
-currencies = {:japan => 'yen', :us => 'dollar', :india => 'repee'}
+currencies = {japan: 'yen', us: 'dollar', india: 'repee'}
 #シンボルを使って値を取り出す
 puts currencies[:us]
-#新しいキーと値の組み合わせを追加する
-currencies[:italy] = 'euro'
