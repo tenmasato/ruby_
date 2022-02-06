b884f3cd48908b42ecda946859f41e5dffd88859
tenmasato <narito1015@icloud.com>
2022-02-06 07:07:47 +0000

    [Start]

---
+++ lesson5/if.rb
@@ -0,0 +1,14 @@
+def buy_burger(menu,drink,potato)
+  #ハンバーガーを購入
+  if drink
+    #ドリンクを購入
+  end
+  if potato
+    #ポテトを購入
+  end
+end
+
+#チーズバーガーとドリンクを購入する
+buy_burger('cheese',true,true)
+#フィッシュバーガーとドリンクを購入する
+buy_burger('fish',true,true)
