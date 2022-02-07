083c09fd4f69a4a2e9bc4178cab41a0bff04313e
tenmasato <narito1015@icloud.com>
2022-02-07 11:12:18 +0000

    [Update]テスト

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -4,5 +4,6 @@ require './lesson5/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 39.37,convert_length(1,'m','in')
+    assert_equal 0.38, convert_length(15,'in','m')
   end
 end
