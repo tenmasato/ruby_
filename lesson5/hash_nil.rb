223f88f11745f79d04a1063c1b0f1141bbf38144
tenmasato <narito1015@icloud.com>
2022-02-11 07:02:32 +0000

    [Finish]

--- lesson5/hash_nil.rb
+++ lesson5/hash_nil.rb
@@ -1,3 +1,12 @@
 #キーがなければ'hello'を返す
 h = Hash.new('hello')
-puts h[:foo]
+puts a = h[:foo]
+puts b = h[:bar]
+
+#変数aと変数ｂは同一オブジェクト
+puts a.equal?(b)
+
+#変数aに破壊的な変更を適用すると,変数の値も一緒に変わってしまう
+a.upcase!
+puts a
+puts b
